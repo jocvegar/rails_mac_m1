b77225dca270b3e44604af64116a45cd6edafd72
Jose Vega <jocvegar@gmail.com>
2021-07-16 12:13:31 -0600

    adds rubocop

---
+++ db/migrate/20210716175452_create_users.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
